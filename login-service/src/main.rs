9f087526dd717e35a68e4ce76ec9df3b360d7e34
AdaShoelace <adashoelace@gmail.com>
2023-07-20 20:20:56 +0200

    login-service: add dependencies

--- login-service/src/main.rs
+++ login-service/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+mod util;
+
+// Crate imports
+use crate::util::init_logging;
+
+// Third party imports
+use anyhow::Result;
+use log::{debug, warn, info, trace, error};
+
+fn main() -> Result<()> {
+    _ = init_logging()?;
+
+    info!("This is info");
+    debug!("This is debug");
+    warn!("This is warn");
+    error!("This is error");
+    trace!("This is trace");
+
+    Ok(())
 }
