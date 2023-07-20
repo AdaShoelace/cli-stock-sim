2110e03d6953d1801afeac955120adb180f6c6fe
AdaShoelace <adashoelace@gmail.com>
2023-07-20 22:35:20 +0200

    login-service: fetch single stock

--- login-service/src/main.rs
+++ login-service/src/main.rs
@@ -3,19 +3,34 @@ mod util;
 // Crate imports
 use crate::util::init_logging;
 
+// Std imports
+use std::env;
+
 // Third party imports
 use anyhow::Result;
-use log::{debug, warn, info, trace, error};
+use log::{debug, error, info, trace, warn};
+use sqlx::postgres::PgPool;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     _ = init_logging()?;
 
-    info!("This is info");
-    debug!("This is debug");
-    warn!("This is warn");
-    error!("This is error");
-    trace!("This is trace");
+    let pool = PgPool::connect(&env::var("DATABASE_URL")?).await?;
+    _ = get_stock(&pool, 2).await;
 
+    Ok(())
+}
+
+async fn get_stock(pool: &PgPool, id: i32) -> Result<()> {
+    let res = sqlx::query!(
+        r#"
+    SELECT * FROM stocks
+    WHERE id=($1)
+        "#,
+        id
+    )
+    .fetch_one(pool)
+    .await?;
+    debug!("{:#?}", res);
     Ok(())
 }
